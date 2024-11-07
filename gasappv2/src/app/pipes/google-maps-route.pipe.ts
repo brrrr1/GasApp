5c0e886d3f9afd3d1ed6cc77ced46ca293424cdb
br1 <delgado.hebru24@triana.salesianos.edu>
2024-11-07 11:59:38 +0100

    Cómo llegar

---
+++ gasappv2/src/app/pipes/google-maps-route.pipe.ts
@@ -0,0 +1,18 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { Gasolinera } from '../models/gas-item.dto';
+
+@Pipe({
+  name: 'googleMapsRoute'
+})
+export class GoogleMapsRoutePipe implements PipeTransform {
+
+  transform(gasolinera: Gasolinera, ...args: unknown[]): unknown {
+    return `https://www.google.com/maps/dir/?api=1&destination=${this.sanitizeValue(gasolinera.latitude)},${this.sanitizeValue(gasolinera.altittude)}`;
+  }
+
+  sanitizeValue(value: string): string {
+    return value.replace(',', '.');
+
+  }
+
+}
